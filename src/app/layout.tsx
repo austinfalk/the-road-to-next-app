0821ef31102c58b221e708ef1f05d6c77a749159
Austin Falk <austin@austinfalk.com>
2025-08-09 15:02:49 -0700

    style(buttons): added button components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
 const geistSans = Geist({
@@ -31,12 +32,18 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Home
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: 'outline' })}
+            >
               Tickets
             </Link>
           </div>
