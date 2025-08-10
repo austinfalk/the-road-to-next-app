c439200b95913557c1dee584358ad712c7cc1dbc
Austin Falk <austin@austinfalk.com>
2025-08-10 00:20:43 -0700

    style(placeholder.tsx): added placeholder page

---
+++ src/components/placeholder.tsx
@@ -0,0 +1,26 @@
+import { LucideMessageSquareWarning } from 'lucide-react';
+import { cloneElement, ReactElement, SVGProps } from 'react';
+
+type PlaceholderProps = {
+  label: string;
+  icon?: ReactElement<SVGProps<SVGSVGElement>>;
+  button?: ReactElement<React.ButtonHTMLAttributes<HTMLButtonElement>>;
+};
+
+const Placeholder = ({
+  label,
+  icon = <LucideMessageSquareWarning />,
+  button = (<button type="button" />) as ReactElement<
+    React.ButtonHTMLAttributes<HTMLButtonElement>
+  >,
+}: PlaceholderProps) => {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-y-2 self-center">
+      {cloneElement(icon, { className: 'w-16 h-16' })}
+      <h2 className="text-center text-lg">{label}</h2>
+      {cloneElement(button, { className: 'h-10' })}
+    </div>
+  );
+};
+
+export { Placeholder };
