955dd49e44dc5210666833d0b78f18e7bec7dfe6
Austin Falk <austin@austinfalk.com>
2025-08-16 15:05:47 -0700

    refactor(ticket-item.tsx): made detail button a separate variable

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,11 +1,7 @@
+import { LucideSquareArrowOutUpRight } from 'lucide-react';
 import Link from 'next/link';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { TICKET_ICONS } from '@/features/ticket/constants';
 import { Ticket } from '@/features/ticket/types';
 import { ticketPath } from '@/paths';
@@ -15,23 +11,31 @@ type TicketItemProps = {
 };
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link href={ticketPath(ticket.id)} className="text-sm">
+        <LucideSquareArrowOutUpRight className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
+
   return (
-    <Card className="w-full max-w-[420px]">
-      <CardHeader>
-        <CardTitle className="flex items-center gap-x-2">
-          <span>{TICKET_ICONS[ticket.status]}</span>
-          <h3 className="truncate text-2xl font-bold">{ticket.title}</h3>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="line-clamp-3 whitespace-break-spaces">{ticket.content}</p>
-      </CardContent>
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-          View
-        </Link>
-      </CardFooter>
-    </Card>
+    <div className="flex w-full max-w-[420px] gap-x-1">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <h3 className="truncate text-2xl font-bold">{ticket.title}</h3>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="line-clamp-3 whitespace-break-spaces">
+            {ticket.content}
+          </p>
+        </CardContent>
+      </Card>
+      <div className="flex flex-col gap-x-1">{detailButton}</div>
+    </div>
   );
 };
 
