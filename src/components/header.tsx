f18bfa162d0b82b2164b8914182eade09bd97442
Austin Falk <austin@austinfalk.com>
2025-08-16 22:58:54 -0700

    feat(theme): dark and light toggle functional

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { LucideKanban } from 'lucide-react';
 import Link from 'next/link';
+import { ThemeSwitcher } from '@/components/theme/theme-switcher';
 import { buttonVariants } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
@@ -7,7 +8,7 @@ const Header = () => {
   return (
     <>
       <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
-        <div>
+        <div className="align-items flex gap-x-2">
           <Link
             href={homePath()}
             className={buttonVariants({ variant: 'ghost' })}
@@ -16,7 +17,8 @@ const Header = () => {
             <h1 className="text-lg font-semibold">TicketBounty</h1>
           </Link>
         </div>
-        <div>
+        <div className="align-items flex gap-x-2">
+          <ThemeSwitcher />
           <Link
             href={ticketsPath()}
             className={buttonVariants({ variant: 'default' })}
