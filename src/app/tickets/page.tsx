26a5c41bda9e8924deba67f16ecce24882866a10
Austin Falk <38740468+austinfalk@users.noreply.github.com>
2025-08-17 20:14:53 -0700

    Feature/server vs client components (#3)
    * feat(tickets): set up async function for fetching tickets
    
    * refactor(logging): added logging for server vs client

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { Heading } from '@/components/heading';
-import { initialTickets } from '@/data';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
+import { getTickets } from '@/features/ticket/queries/get-tickets';
+import { Ticket } from '@/features/ticket/types';
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const result = await getTickets();
+      setTickets(result);
+    };
+
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading
@@ -11,7 +26,7 @@ const TicketsPage = () => {
       />
 
       <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
