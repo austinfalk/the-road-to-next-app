71623c25651d87d4a06df5f86ce327167238e4a4
Austin Falk <austin@austinfalk.com>
2025-10-24 15:38:53 -0700

    refactor(next): nextjs 16 update and bun runtime

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,11 @@
-import { FlatCompat } from '@eslint/eslintrc';
+// @ts-nocheck
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import prettierConfig from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...nextCoreWebVitals,
+  prettierConfig,
   {
     plugins: {
       'simple-import-sort': simpleImportSort,
